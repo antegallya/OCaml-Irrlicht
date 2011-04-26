0646c5878c7cfdd39f8a320278f4f1536b8f95c4
axiles <alexis.bernadet@noos.fr>
2011-04-26 10:00:13 +0200

    Add water in example 07

--- examples/07/main.ml
+++ examples/07/main.ml
@@ -4,6 +4,18 @@ let add_room (driver : Irr_video.driver) (smgr : Irr_scene.manager) =
   node#set_material_texture (driver#get_texture "../../media/wall.jpg");
   node#set_material_flag `lighting false
 
+let add_water (driver : Irr_video.driver) (smgr : Irr_scene.manager) =
+  let mesh = smgr#add_hill_plane_mesh "myHill" (20., 20.)
+    ~texture_repeat_count:(10., 10.) (40, 40) in
+  let node = smgr#add_water_surface_node ~wave_height:3. ~wave_length:30.
+    (mesh#mesh 0) in
+  node#set_pos (0., 7., 0.);
+  node#set_material_texture (driver#get_texture "../../media/stones.jpg");
+  let water_texture = driver#get_texture "../../media/water.jpg" in
+  node#set_material_texture ~layer:1 water_texture;
+  node#set_material_type `reflection_2_layer;
+  node#set_material_flag `lighting false
+
 let add_camera (device : Irr.device) (smgr : Irr_scene.manager) =
   let camera = smgr#add_camera_fps () in
   camera#set_pos (-50., 50., -150.);
@@ -13,6 +25,7 @@ let () =
   let device = Irr.create_device ~dtype:`opengl () in
   let driver = device#driver and smgr = device#scene_manager in
   add_room driver smgr;
+  add_water driver smgr;
   add_camera device smgr;
   while device#run do
     driver#begin_scene ();
