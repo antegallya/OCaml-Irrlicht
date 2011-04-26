7d8f64515556ee5789ffa43a4657dee1937b5107
axiles <alexis.bernadet@noos.fr>
2011-04-26 10:22:20 +0200

    Add lighting ball in example 07

--- examples/07/main.ml
+++ examples/07/main.ml
@@ -16,6 +16,18 @@ let add_water (driver : Irr_video.driver) (smgr : Irr_scene.manager) =
   node#set_material_type `reflection_2_layer;
   node#set_material_flag `lighting false
 
+let add_lighting_ball (driver : Irr_video.driver) (smgr : Irr_scene.manager) =
+  let light_node = smgr#add_light_node ~color:(Irr_core.colorf_RGB 1. 0.6 0.7)
+    ~radius:800. () in
+  let anim = smgr#create_fly_circle ~center:(0., 150., 0.) ~radius:250. () in
+  light_node#add_animator anim;
+  let bill_node = smgr#add_billboard_node ~parent:light_node ~size:(50., 50.)
+    () in
+  bill_node#set_material_flag `lighting false;
+  bill_node#set_material_type `transparent_add_color;
+  let particle_texture = driver#get_texture "../../media/particlewhite.bmp" in
+  bill_node#set_material_texture particle_texture
+  
 let add_camera (device : Irr.device) (smgr : Irr_scene.manager) =
   let camera = smgr#add_camera_fps () in
   camera#set_pos (-50., 50., -150.);
@@ -26,6 +38,7 @@ let () =
   let driver = device#driver and smgr = device#scene_manager in
   add_room driver smgr;
   add_water driver smgr;
+  add_lighting_ball driver smgr;
   add_camera device smgr;
   while device#run do
     driver#begin_scene ();
