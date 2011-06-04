33ebb330b049fe625ffde1e11cdab7f6cc84b0b7
axiles <alexis.bernadet@noos.fr>
2011-06-04 15:15:29 +0200

    Add module Colour_func in example 09

--- examples/09/main.ml
+++ examples/09/main.ml
@@ -1,1 +1,15 @@
-let () = ()
+module Colour_func : sig
+  type t = float -> float -> float -> Irr_core.color
+  val grey : t
+  val yellow : t
+  val white : t
+end = struct
+  type t = float -> float -> float -> Irr_core.color
+  let grey x y z =
+    let n = int_of_float (255. *. z) in
+    Irr_core.color_ARGB 255 n n n
+  let yellow x y z =
+    let aux w = 128 + int_of_float (127. *. w) in
+    Irr_core.color_ARGB 255 (aux x) (aux y) 255
+  let white x y z = Irr_core.color_ARGB 255 255 255 255
+end
