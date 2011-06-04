390dac2f3223965ea971b2d6030210d6135bb960
axiles <alexis.bernadet@noos.fr>
2011-06-04 15:25:20 +0200

    Add module Generate_func in example 09

--- examples/09/main.ml
+++ examples/09/main.ml
@@ -13,3 +13,27 @@ end = struct
     Irr_core.color_ARGB 255 (aux x) (aux y) 255
   let white x y z = Irr_core.color_ARGB 255 255 255 255
 end
+
+module Generate_func : sig
+  type t = int -> int -> float -> float
+  val eggbox : t
+  val moresine : t
+  val justexp : t
+end = struct
+  type t = int -> int -> float -> float
+  let eggbox x y s =
+    let r = 4. *. sqrt (float_of_int (x * x + y * y)) /. s in
+    let aux z = cos (0.2 *. float_of_int x) in
+    let z = exp (-. r *. 2.) *. (aux x) *. (aux y) in
+    0.25 +. 0.25 *. z
+  let moresine x y s =
+    let xx = 0.3 *. (float_of_int x) /. s in
+    let yy = 12. *. (float_of_int y) /. s in
+    let z = sin (xx *. xx +. yy) *. sin (xx +. yy *. yy) in
+    0.25 +. 0.25 *. z
+  let justexp x y s =
+    let aux z = 6. *. (float_of_int z) /. s in
+    let xx = aux x and yy = aux y in
+    let z = xx *. xx +. yy *. yy in
+    0.3 *. z *. cos (xx *. yy)
+end
