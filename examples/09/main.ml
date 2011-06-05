54898e4d0be76390de9cacf8fd67bf9c130ae60c
axiles <alexis.bernadet@noos.fr>
2011-06-05 14:51:17 +0200

    Change the color of the light

--- examples/09/main.ml
+++ examples/09/main.ml
@@ -168,7 +168,7 @@ let () =
   let mesh_node = smgr#add_mesh_node (Mesh.get_mesh mesh :> Irr_scene.mesh) in
   mesh_node#set_material_flag `back_face_culling false;
   let node = smgr#add_light_node ~pos:(0., 100., 0.)
-    ~color:(Irr_core.colorf_RGB 0.6 0.7 1.) ~radius:500. () in
+    ~color:(Irr_core.colorf_RGB 1. 0.6 0.7) ~radius:500. () in
   let light_att = (0., 1. /. 500., 0.) in
   node#set_data (Irr_video.Light.cons node#data ~attenuation:light_att ());
   let anim = smgr#create_fly_circle ~center:(0., 150., 0.) ~radius:250. () in
