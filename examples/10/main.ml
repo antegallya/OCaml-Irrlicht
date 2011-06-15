530e6b821eec1c5d2675e3eb146edc9cfe9de9fe
axiles <alexis.bernadet@noos.fr>
2011-06-15 17:18:44 +0200

    Add buttons "New Window" and "Open Files" in example 10

--- examples/10/main.ml
+++ examples/10/main.ml
@@ -4,6 +4,10 @@ let () =
   let env = device#gui_env in
   let _ = env#add_button ~text:"Quit" ~tooltiptext:"Exits program"
     (10, 240, 110, 240 + 32) in
+  let _ = env#add_button ~text:"New window" ~tooltiptext:"Launches a new Window"
+    (10, 280, 110, 280 + 32) in
+  let _ = env#add_button ~text:"File open" ~tooltiptext:"Opens a file"
+    (10, 320, 110, 320 + 32) in
   while device#run do
     driver#begin_scene ();
     env#draw_all;
