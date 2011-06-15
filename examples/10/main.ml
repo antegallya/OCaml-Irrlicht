8746e069fb318ed6c8a45e6282a17dc5ce54ebaf
axiles <alexis.bernadet@noos.fr>
2011-06-15 17:13:52 +0200

    Add button Quit in example 10

--- examples/10/main.ml
+++ examples/10/main.ml
@@ -2,6 +2,8 @@ let () =
   let device = Irr.create_device ~dtype:`opengl () in
   let driver = device#driver in
   let env = device#gui_env in
+  let _ = env#add_button ~text:"Quit" ~tooltiptext:"Exits program"
+    (10, 240, 110, 240 + 32) in
   while device#run do
     driver#begin_scene ();
     env#draw_all;
